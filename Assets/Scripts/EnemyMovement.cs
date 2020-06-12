1e82cd18753fd67ed6a363c7d495947e19a1e680
MarcusD96 <marc_not_listening@hotmail.com>
2020-06-12 16:32:18 -0400

    Init + Map + Enemy + Enemy Pathing

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+    private Transform target;
+    private int wayPointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        target = Path.waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.4f) {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint() {
+        if(wayPointIndex >= Path.waypoints.Capacity - 1) {
+            Destroy(gameObject);
+            return;
+        }
+        wayPointIndex++;
+        target = Path.waypoints[wayPointIndex];
+    }
+}
---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+    public static List<Transform> waypoints = new List<Transform>();
+
+    void Awake() {
+        waypoints.Capacity = transform.childCount;
+        for(int i = 0; i < waypoints.Capacity; i++) {
+            waypoints.Add(transform.GetChild(i));
+        }
+    }
+
+}
