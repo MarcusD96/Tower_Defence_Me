b1561c70004c638b21ea719fadd7499ab170dfea
MarcusD96 <marc_not_listening@hotmail.com>
2020-06-12 17:04:17 -0400

    added tmpro, enemies spawn in waves + countdown timer ui

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    private float countDown = 2.5f, timeBetweenWaves = 5.5f;
+
+    public TextMeshProUGUI countdownText;
+
+    private int waveIndex = 0;
+
+    void Update() {
+        if(countDown <= 0.0f) {
+            StartCoroutine(SpawnWave(0.5f));
+            countDown = timeBetweenWaves;
+            return;
+        }
+
+        countDown -= Time.deltaTime;
+
+        countdownText.text = Mathf.Round(countDown).ToString();
+    }
+
+    IEnumerator SpawnWave(float spawnTime) {
+        for(int i = 0; i < waveIndex; i++) {
+            SpawnEnemy();
+            yield return new WaitForSeconds(spawnTime);
+        }
+        waveIndex++;
+    }
+
+    void SpawnEnemy() {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
