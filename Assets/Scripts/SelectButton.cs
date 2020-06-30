1677fcb68c1e9a5345549f8f10c95648a1af2f8f
MarcusD96 <marc_not_listening@hotmail.com>
2020-06-30 13:04:02 -0400

    prepare for unity update

---
+++ Assets/Scripts/SelectButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectButton : MonoBehaviour {
+
+    public Button button;
+    public Image selectedBorder;
+    public Color enabledBorder, disabledBorder;
+
+    void Start() {
+        button.onClick.AddListener(Hightlight);
+    }
+
+    void Hightlight() {
+        selectedBorder.color = 
+    }
+}
